cbb764395acba868cf65512c3fcb57d559413af9
Prayas Raj <97390918+Prayas7632@users.noreply.github.com>
2024-11-30 15:57:46 +0530

    Home Page Layout

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import SelectMovie from '../Components/SelectMovie'
+import TimeSchedule from '../Components/TimeSchedule'
+import SelectSeat from '../Components/SelectSeat'
 
 const Home = () => {
   return (
-    <div>
+    <div className='container'>
+      <div className='wrapper'>
         <div className='select_movie_component'>
-            <SelectMovie/>
+          <SelectMovie />
         </div>
+        <div className='last_booking_details_container'>
+          <LastBooking/>
+        </div>
+      </div>
+      <div className='time_seats_container'>
+        <TimeSchedule/>
+        <SelectSeat/>
+        <button> Book Now </button>
+      </div>
     </div>
   )
 }
