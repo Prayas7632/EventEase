1105bd7d569c542c3c69c5139c6a560b75c1e1f7
Prayas Raj <97390918+Prayas7632@users.noreply.github.com>
2024-12-03 21:38:59 +0530

    Data exported

--- front-end/src/Data.js
+++ front-end/src/Data.js
@@ -3,3 +3,4 @@ let moviesList = ["Tenet" , "Shutter Island" , "Inception", "Fast X" , "Matrix"]
 let slots = ['10:00 AM', '01:00 PM', '03:00 PM', '08:00 PM']
 
 exports.moviesList = moviesList;
+exports.slots = slots;
