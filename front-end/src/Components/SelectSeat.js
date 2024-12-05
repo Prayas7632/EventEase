dbb3bc081580d56691a716ba5e3b5ab5534e2fd8
Prayas Raj <97390918+Prayas7632@users.noreply.github.com>
2024-12-05 11:27:11 +0530

    Seat Select

--- front-end/src/Components/SelectSeat.js
+++ front-end/src/Components/SelectSeat.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { seats } from '../Data'
+import SeatInputs from './SeatInputs';
+import '../Css/SelectSeats.css'
 
 const SelectSeat = () => {
+  console.log(seats);
   return (
-    <div>SelectSeat</div>
+    <div>
+      <div className='SS_wrapper'>
+        <h1 className='SS_heading'>Select Seats : </h1>
+        <div className='SS_main_container'>
+          {seats.map((el, index) => 
+            <SeatInputs key={index} text={el} />
+          )}
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default SelectSeat
+export default SelectSeat;
