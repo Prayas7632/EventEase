35b099f209c2ac71684a26eb0ccc4dd6ec93044e
Prayas Raj <97390918+Prayas7632@users.noreply.github.com>
2024-12-06 12:33:15 +0530

    Booking Details

--- front-end/src/Components/LastBooking.js
+++ front-end/src/Components/LastBooking.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { seats } from '../Data'
+import '../Css/LastBookingDetails.css'
 
 const LastBooking = () => {
   return (
-    <div>
-        LastBooking
+    <div className='last_booking_datails_container_main'>
+      <h2 className='last_booking_details_header'>Last Booking</h2>
+      <div className='seats_container'>
+        <p className='seats_header'>Seats :</p>
+        <ul className='seats'>
+          {seats.map((seats, index) => {
+            return <li className='seat_value' key={index}>
+              {seats} : 0
+            </li>
+          })}
+        </ul>
+      </div>
+      <p className='slot' style={{textAlign:'left'}}>Slot : 00:00 AM <span></span></p>
+      <p className='movie'> Movie : Tenet</p>
     </div>
   )
 }
