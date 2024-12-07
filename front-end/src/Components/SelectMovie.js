0cc84cbc3d9099f9ceb444f588cacd425f623955
Prayas Raj <97390918+Prayas7632@users.noreply.github.com>
2024-12-07 10:52:57 +0530

    context added

--- front-end/src/Components/SelectMovie.js
+++ front-end/src/Components/SelectMovie.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { moviesList } from '../Data'
 import RadioComponent from './RadioComponent'
 import '../Css/SelectMovie.css'
+import BsContext from '../Context/BsContext'
 
 const SelectMovie = () => {
+    const context = useContext(BsContext);
+
+    const {movie, changeMovie} = context
+    const handleChangeMovie = (val) => {
+        changeMovie(val)
+
+        window.localStorage.setItem("movie", val)
+    }
+
     return (
         <div>
             <h1 className='SM_heading'>
@@ -11,7 +21,7 @@ const SelectMovie = () => {
             </h1>
             <div className='SM_main_container'>
                 {moviesList.map((el, index) => {
-                    return <RadioComponent text={el} key={index} />
+                    return <RadioComponent text={el} key={index} data={movie} changeSelection = {handleChangeMovie}/>
                 })}
             </div>
         </div>
