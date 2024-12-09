115668d1f2ec119118ffa1bdb07e77a86b698e97
Prayas Raj <97390918+Prayas7632@users.noreply.github.com>
2024-12-09 13:05:55 +0530

    Seat Selection

--- front-end/src/Components/SelectSeat.js
+++ front-end/src/Components/SelectSeat.js
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { seats } from '../Data'
 import SeatInputs from './SeatInputs';
 import '../Css/SelectSeats.css'
+import BsContext from '../Context/BsContext';
 
 const SelectSeat = () => {
-  console.log(seats);
+  const context = useContext(BsContext);
+  const {noOfSeat, changeNoOfSeats} = context;
+
   return (
     <div>
       <div className='SS_wrapper'>
         <h1 className='SS_heading'>Select Seats : </h1>
         <div className='SS_main_container'>
           {seats.map((el, index) => 
-            <SeatInputs key={index} text={el} />
+            <SeatInputs key={index} text={el} noOfSeat = {noOfSeat} changeNoOfSeats = {changeNoOfSeats}/>
           )}
         </div>
       </div>
