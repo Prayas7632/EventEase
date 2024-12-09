2d2547947127768b84777da8f2654edbc0ae45ce
Prayas Raj <97390918+Prayas7632@users.noreply.github.com>
2024-12-09 13:07:26 +0530

    Added Time Scheduling-LocalStorage

--- front-end/src/Components/TimeSchedule.js
+++ front-end/src/Components/TimeSchedule.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { slots } from '../Data'
 import RadioComponent from './RadioComponent'
 import '../Css/TimeSchedule.css'
+import BsContext from '../Context/BsContext'
 
 const TimeSchedule = () => {
+
+  const context = useContext(BsContext);
+
+  const {time, changeTime} = context;
+
+  const handleChangeItem = (val) => {
+    changeTime(val)
+
+    window.localStorage.setItem("slot", val)
+  }
+
   return (
     <div>
       <div className='Slot_container'>
         <h1 className='TS_heading'>Select a Schedule</h1>
         <div className='TS_main_container'>{slots.map((el, index) => {
           return (
-            <RadioComponent text={el} key={index} />
+            <RadioComponent text={el} key={index} data = {time} changeSelection={handleChangeItem}/>
           )
         })}</div>
       </div>
