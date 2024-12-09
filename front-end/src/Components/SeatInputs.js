267e15b9f20604bbe8196579f4c03f34f424ca88
Prayas Raj <97390918+Prayas7632@users.noreply.github.com>
2024-12-09 13:05:25 +0530

    Passed seats to local Storage

--- front-end/src/Components/SeatInputs.js
+++ front-end/src/Components/SeatInputs.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import '../Css/SeatInput.css'
 
-const SeatInputs = ({key, text}) => {
+const SeatInputs = ({key, text, noOfSeat, changeNoOfSeats}) => {
+
+  const change_seats = (e) =>{
+    changeNoOfSeats({...noOfSeat,[e.target.name]:Number(e.target.value)})
+
+    window.localStorage.setItem("seats",
+      JSON.stringify({
+        ...noOfSeat, [e.target.name]:Number(e.target.value)
+      })
+    )
+  }
+
   return (
     <div className='form-check-label seats'>
         <span className='text'>{text}</span>
-        <input type='number' className='seat-input' placeholder='0' max = "30" min = "0"></input>
+        <input type='number' className='seat-input' placeholder='0' max = "30" min = "0" name = {text} onChange={change_seats} value = {noOfSeat[text]}></input>
     </div>
   )
 }
