718aa991a216bc5f06e7bfe60a594c2b11fa6cb3
Prayas Raj <97390918+Prayas7632@users.noreply.github.com>
2024-12-12 11:50:54 +0530

    Handled Booking

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import SelectMovie from '../Components/SelectMovie'
 import TimeSchedule from '../Components/TimeSchedule'
 import SelectSeat from '../Components/SelectSeat'
 import LastBooking from '../Components/LastBooking'
 import '../Css/Home.css'
 import '../Css/LastBookingDetails.css'
+import BsContext from '../Context/BsContext'
 
 const Home = () => {
 
+  const context = useContext(BsContext)
+
+  const {movie, time, noOfSeat, handlePostBooking, setErrorPopup, setErrorMessage,} = context
+
   const handleBookNow = () => {
-    
+    if(!movie){
+      setErrorPopup(true)
+      setErrorMessage("Please Select a Movie")
+    }
+    if(!time){
+      setErrorPopup(true)
+      setErrorMessage("Please Select time slot")
+    }
+    if(!noOfSeat){
+      setErrorPopup(true)
+      setErrorMessage("Please Select no of seats")
+    }else{
+      handlePostBooking()
+    }
   }
 
   return (
