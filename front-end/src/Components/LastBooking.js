669138aea54432035213037b8fcec0f0b8986768
Prayas Raj <97390918+Prayas7632@users.noreply.github.com>
2024-12-12 12:58:45 +0530

    Last Booking Displayed

--- front-end/src/Components/LastBooking.js
+++ front-end/src/Components/LastBooking.js
@@ -20,15 +20,15 @@ const LastBooking = () => {
             <div className='seats_container'>
               <p className='seats_header'>Seats :</p>
               <ul className='seats'>
-                {seats.map((seats, index) => {
+                {seats.map((seat, index) => {
                   return <li className='seat_value' key={index}>
-                    (seat) : {NoOfSeats}
+                    {seat} : {LastBookingDetails.seats[seat]}
                   </li>
                 })}
               </ul>
             </div>
-            <p className='slot' style={{ textAlign: 'left' }}>Slot : 00:00 AM <span></span></p>
-            <p className='movie'> Movie : <span>Tenet</span></p>
+            <p className='slot' style={{ textAlign: 'left' }}>Slot : <span>{LastBookingDetails.slot}</span></p>
+            <p className='movie'> Movie : <span>{LastBookingDetails.movie}</span></p>
           </>
         ) : (
           <p className='no_previous'>No previous Booking</p>
