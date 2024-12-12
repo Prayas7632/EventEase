e2749e6324115183aaea98ffc939fcee3c9728bf
Prayas Raj <97390918+Prayas7632@users.noreply.github.com>
2024-12-12 12:15:52 +0530

    Showing Data on the client Side

--- front-end/src/Components/LastBooking.js
+++ front-end/src/Components/LastBooking.js
@@ -1,23 +1,40 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import { seats } from '../Data'
 import '../Css/LastBookingDetails.css'
+import BsContext from '../Context/BsContext'
 
 const LastBooking = () => {
+  const context = useContext(BsContext)
+
+  const { LastBookingDetails, handleGetBooking } = context
+  useEffect(() => {
+    handleGetBooking()
+  }, [])
+
   return (
     <div className='last_booking_datails_container_main'>
-      <h2 className='last_booking_details_header'>Last Booking</h2>
-      <div className='seats_container'>
-        <p className='seats_header'>Seats :</p>
-        <ul className='seats'>
-          {seats.map((seats, index) => {
-            return <li className='seat_value' key={index}>
-              {seats} : 0
-            </li>
-          })}
-        </ul>
-      </div>
-      <p className='slot' style={{textAlign:'left'}}>Slot : 00:00 AM <span></span></p>
-      <p className='movie'> Movie : Tenet</p>
+      <h2 className='last_booking_details_header'>Last Booking : </h2>
+      {
+        LastBookingDetails ? (
+          <>
+            <div className='seats_container'>
+              <p className='seats_header'>Seats :</p>
+              <ul className='seats'>
+                {seats.map((seats, index) => {
+                  return <li className='seat_value' key={index}>
+                    (seat) : {NoOfSeats}
+                  </li>
+                })}
+              </ul>
+            </div>
+            <p className='slot' style={{ textAlign: 'left' }}>Slot : 00:00 AM <span></span></p>
+            <p className='movie'> Movie : <span>Tenet</span></p>
+          </>
+        ) : (
+          <p className='no_previous'>No previous Booking</p>
+        )
+      }
+
     </div>
   )
 }
