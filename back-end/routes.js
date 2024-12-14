c0ebd321b4503b4067f2f53ea3ab615566540c2b
Prayas Raj <97390918+Prayas7632@users.noreply.github.com>
2024-12-14 21:22:18 +0530

    Fixed routes

--- back-end/routes.js
+++ back-end/routes.js
@@ -9,37 +9,38 @@ const app = express()
 router.use(express.json())
 router.use(cors())
 
-router.post("/booking", async(req, res) => {
-    const {movie, slot, seats} = req.body
+router.post("/booking", async (req, res) => {
+    const { movie, slot, seats } = req.body
+    console.log(movie, slot, seats)
 
-    try{
-        const myData = new Ticket({movie, slot, seats})
+    try {
+        const myData = new Ticket({ movie, slot, seats })
 
         const saved = await myData.save()
 
-        res.status(200).json({data:myData, message:"Booking Successful"})
-    }catch(err){
+        res.status(200).json({ data: myData, message: "Booking Successful" })
+    } catch (err) {
         res.status(500).json({
-            data:null,
-            message:"something went wrong !please try Again"
+            data: null,
+            message: "something went wrong !please try Again"
         })
     }
 })
 
-router.get("/booking", async(req,res) => {
-    try{
-        const myData = await Ticket.find().sort({_id:-1}).limit(1)
+router.get("/booking", async (req, res) => {
+    try {
+        const myData = await Ticket.find().sort({ _id: -1 }).limit(1)
 
-        if(myData.length === 0){
-            res.status(200).json({data:null, message:"No previous booking found"})
-        }else{
-            res.status(200).json({data:myData[0]})
+        if (myData.length === 0) {
+            res.status(200).json({ data: null, message: "No previous booking found" })
+        } else {
+            res.status(200).json({ data: myData[0] })
         }
-    }catch(error){
+    } catch (error) {
         res.status(500).json({
-            data:null,
+            data: null,
             error,
-            message:"Something went wrong!"
+            message: "Something went wrong!"
         })
     }
 })
