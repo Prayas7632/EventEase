147c4d6e42a295c3a093fc47692cd6e6316a623c
Prayas Raj <97390918+Prayas7632@users.noreply.github.com>
2024-12-15 21:23:55 +0530

    display booking

--- front-end/src/Components/LastBooking.js
+++ front-end/src/Components/LastBooking.js
@@ -6,29 +6,29 @@ import BsContext from '../Context/BsContext'
 const LastBooking = () => {
   const context = useContext(BsContext)
 
-  const { LastBookingDetails, handleGetBooking } = context
+  const { lastbookingDetails, handleGetBooking } = context
   useEffect(() => {
     handleGetBooking()
-  })
+  }, [])
 
   return (
     <div className='last_booking_datails_container_main'>
       <h2 className='last_booking_details_header'>Last Booking : </h2>
       {
-        LastBookingDetails ? (
+        lastbookingDetails ? (
           <>
             <div className='seats_container'>
               <p className='seats_header'>Seats :</p>
               <ul className='seats'>
                 {seats.map((seat, index) => {
                   return <li className='seat_value' key={index}>
-                    {seat} : {LastBookingDetails.seats[seat]}
+                    {seat} : {lastbookingDetails.seats[seat]}
                   </li>
                 })}
               </ul>
             </div>
-            <p className='slot' style={{ textAlign: 'left' }}>Slot : <span>{LastBookingDetails.slot}</span></p>
-            <p className='movie'> Movie : <span>{LastBookingDetails.movie}</span></p>
+            <p className='slot' style={{ textAlign: 'left' }}>Slot : <span>{lastbookingDetails.slot}</span></p>
+            <p className='movie'> Movie : <span>{lastbookingDetails.movie}</span></p>
           </>
         ) : (
           <p className='no_previous'>No previous Booking</p>
